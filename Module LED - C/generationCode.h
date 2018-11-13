fbcb70ec6796a6813e6413a56700977d1d9c9720
Meta-Don <44459141+Meta-Don@users.noreply.github.com>
2018-11-13 12:36:19 +0100

    Add files via upload

---
+++ Module LED - C/generationCode.h
@@ -0,0 +1,9 @@
+#ifndef GENERATION_CODE_H
+#define GENERATION_CODE_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void creation_param_h(void);
+
+#endif // GENERATION_CODE_H
---
+++ Module LED - C/param.h
@@ -0,0 +1,10 @@
+const byte IR = 0; // photo-transistor PIN A0
+byte mode = 0; // Mode d'allumage LED
+byte nLED = 9; //Nombre de LED
+
+/* MODE D'ECLAIRAGE DES LEDS
+0 : All
+1 : Une sur deux            
+3 : Chenille
+4 : Vague
+*/
