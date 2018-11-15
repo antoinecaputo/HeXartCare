f8c03638e1e24e0af9fc2ef568a93672776e547b
Meta-Don <44459141+Meta-Don@users.noreply.github.com>
2018-11-15 21:42:48 +0100

    Ajout d'un switch pour les choix sur donnees.c
    Pour rendre ça plus interactif.
    
    À compléter avec les fonctions d'actions.c

--- Module_Traitement_Donnees/main.c
+++ Module_Traitement_Donnees/main.c
@@ -4,7 +4,51 @@
 
 int main()
 {
-    nombre_lignes_csv();
-    donnees_vers_structures();
+    int choix_csv,check=0;
+    do
+    {
+        printf("\nBienvenue.\nQue voulez-vous faire ?\n \
+            0: Fermer le gestionnaire de .csv\n \
+            1: Lire le fichier .csv\n \
+            2: Afficher le nombre de lignes du fichier .csv\n \
+            3: Transferer les données du .csv vers la structure de données\n");
+        printf("\n   > ");
+        scanf("%d",&choix_csv);
+
+        //évite de choisir plusieurs fois de transférer dans la structure, écrasement de données inutile
+        if(choix_csv==3 && check==1)
+        {
+            printf("\nLes donnees ont deja ete transferees, action impossible.\n");
+            choix_csv=4;
+        }
+        switch(choix_csv)
+        {
+            case 0:
+                exit(0);
+                break;
+            case 1:
+                lire_csv();
+                break;
+            case 2:
+                nombre_lignes_csv();
+                break;
+            case 3:
+                placer_dans_struct();
+                check=1;
+                break;
+            default:
+                printf("\nErreur lors de la saisie, veuillez recommencer.\n");
+                break;
+        }
+    }while(choix_csv!=0);
+    /*
+    int choix_traitement;
+
+    do
+    {
+        printf("\n")
+    }while(choix_traitement!=0);
+    */
+
     return 0;
 }
