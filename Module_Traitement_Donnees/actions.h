362d97ab18e8b821ff095f221efaf2883991bf86
Meta-Don <44459141+Meta-Don@users.noreply.github.com>
2018-11-16 13:20:54 +0100

    ajout de la recherche

--- Module_Traitement_Donnees/actions.h
+++ Module_Traitement_Donnees/actions.h
@@ -3,11 +3,22 @@
 
 #include "donnees.h"
 
+//fonction pour transférer un tableau de structures vers un tableau temporaire et inversement
 void transferer_tab_struct(int*,int,int);
+
+//calcul de la moyenne des pouls
+float calcul_moyenne_pouls(void);
+
+//affiche le minimum et le maximum des pouls de la structure de donnees
+void min_et_max(void);
+
+//fonctions du tri croissant/décroissant utilisant la méthode du tri fusion
 void tri_fusion(int*,int,int);
 void fusionner_croissant(int*,int*,int*,int);
 void fusionner_decroissant(int*,int*,int*,int);
 
-// INSERER ALGORITHME DE RECHERCHE void recherche(PARAMETRES);
+//fonction de recherche linéaire
+int recherche_lineaire(int);
+
 
 #endif // ACTIONS_H
