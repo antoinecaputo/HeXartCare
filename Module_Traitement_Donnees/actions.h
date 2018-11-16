a547730b53ef282204e0482a7286634f493157d1
Meta-Don <44459141+Meta-Don@users.noreply.github.com>
2018-11-16 09:00:31 +0100

    Ajout tri décroissant

--- Module_Traitement_Donnees/actions.h
+++ Module_Traitement_Donnees/actions.h
@@ -4,8 +4,9 @@
 #include "donnees.h"
 
 void transferer_tab_struct(int*,int,int);
-void tri_fusion_croissant(int*,int);
+void tri_fusion(int*,int,int);
 void fusionner_croissant(int*,int*,int*,int);
+void fusionner_decroissant(int*,int*,int*,int);
 
 // INSERER ALGORITHME DE RECHERCHE void recherche(PARAMETRES);
 
