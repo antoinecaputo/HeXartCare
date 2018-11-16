25562431feb5cca3d5b50447164b4bd1cde70f4b
antoinecaputo <44469196+antoinecaputo@users.noreply.github.com>
2018-11-16 12:15:49 +0100

    Update generationCode.c

--- M2/LED/generationCode.c
+++ M2/LED/generationCode.c
@@ -2,14 +2,14 @@
 
 void creation_param_h(void)
 {
-    //Création et édition du fichier param.h
+    //CrÃ©ation et Ã©dition du fichier param.h
 
-    FILE *parametre_ptr; //création du pointeur vers le fichier param.h
+    FILE *parametre_ptr; //crÃ©ation du pointeur vers le fichier param.h
 
-    parametre_ptr=fopen("param.h","w"); //création de param.h
+    parametre_ptr=fopen("param.h","w"); //crÃ©ation de param.h
 
     fprintf(parametre_ptr,"const byte IR = 0; // photo-transistor PIN A0\n"); //ligne 1
-    fprintf(parametre_ptr,"byte mode = 0; // Mode d'allumage LED\n"); //ligne 2
+    fprintf(parametre_ptr,"byte mode = \" + mode + \";"; // Mode d'allumage LED\n"); //ligne 2
     fprintf(parametre_ptr,"byte nLED = 9; //Nombre de LED\n"); //ligne 3
     fprintf(parametre_ptr,"\n/* MODE D'ECLAIRAGE DES LEDS\n0 : All\n1 : Une sur deux\
             \n3 : Chenille\n4 : Vague\n*/"); //ligne 4
