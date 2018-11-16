412459325a24368294f1df354d958475250ce6a4
Meta-Don <44459141+Meta-Don@users.noreply.github.com>
2018-11-16 13:32:35 +0100

    Update main.c

--- G-n-rateur_donn-es/main.c
+++ G-n-rateur_donn-es/main.c
@@ -12,21 +12,16 @@ typedef struct echantillon_aleatoire
 enum taille_frequence
 {
     //pour un adulte, au repos
-    lowest_pulse=10,
-    highest_pulse=70,
+    lowest_pulse=50,
+    highest_pulse=80,
     echantillon=300, //nombre de personnes
 };
 
 enum temps_int
 {
-    max_heure=23,
-    min_heure=0,
-
-    max_minute=60,
-    min_minute=0,
-
-    max_seconde=60,
-    min_seconde=0,
+    //valeurs arbitraires
+    max_tps=1000;
+    min_tps=0;
 };
 
 void convert_to_csv(E*);
@@ -42,7 +37,7 @@ int main()
     for(i=0;i<echantillon;i++)
     {
         e[i].freq=(rand()% (highest_pulse-lowest_pulse + 1))+lowest_pulse;
-        e[i].tps=(rand()% (max_heure - min_heure + 1))+min_heure;
+        e[i].tps=(rand()% (max_tps - min_tps + 1))+min_tps;
         printf("Personne %d :\n\nFrequence cardiaque : %d\tTemps : %d ms\n\n",i+1,e[i].freq,e[i].tps);
     }
 
@@ -58,19 +53,15 @@ void convert_to_csv(E *e)
 
     //création du fichier csv
     FILE *pointeur_csv;
-    pointeur_csv=fopen("donnees_aleatoires_v7.csv","w");
-
-    //titre du fichier
-    fprintf(pointeur_csv,"Personne;Frequence;Temps(ms)");
+    pointeur_csv=fopen("donnees_aleatoires.csv","w");
 
     //ajout des données de la structure au fichier
     int j;
     for(j=0;j<echantillon;j++)
     {
         //printf("Ajout des elements en cours..."); //test
-        fprintf(pointeur_csv,"\nPersonne_%d",j+1);
-        fprintf(pointeur_csv,";%d",e[j].freq);
-        fprintf(pointeur_csv,";%d",e[j].tps);
+        fprintf(pointeur_csv,"%d",e[j].freq);
+        fprintf(pointeur_csv,";%d\n",e[j].tps);
     }
 
     //fermeture du fichier
